2f2f353e73e21784325d5f5d093823f033b28716
Pedro <pedrodelgado81@gmail.com>
2025-11-20 00:01:00 +0100

    Esqueleto springboot

---
+++ src/main/java/es/informatica/qaproject/QaprojectApplication.java
@@ -0,0 +1,13 @@
+package es.informatica.qaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaprojectApplication.class, args);
+	}
+
+}
